b1622de06dad535fd4bb6db8b85d6c6cfc5b3477
patkyu <yourstrulypatkyu@gmail.com>
2023-09-04 21:21:39 +0800

    COMPLETE EXCEPT ADMIN

---
+++ sofbox-dashboard-lite/html/js1/bookpage.js
@@ -0,0 +1,17 @@
+const confirmButton = document.getElementById('confirmButton');
+const modal = document.getElementById('myModal');
+const closeModal = document.getElementById('closeModal');
+
+confirmButton.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+
+closeModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+});
